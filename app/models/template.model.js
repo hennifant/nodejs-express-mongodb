cfbd925c01150afa8e5672477423309df716b2c4
hennifant <henning.schwichow@gmx.de>
2022-11-17 12:45:45 +0100

    Fixes

--- app/models/template.model.js
+++ app/models/template.model.js
@@ -1,15 +1,17 @@
 module.exports = (mongoose) => {
-  const Template = mongoose.model(
-    "template",
-    mongoose.Schema(
-      {
-        title: String,
-        description: String,
-        published: Boolean,
-      },
-      { timestamps: true }
-    )
-  );
+  var schema = mongoose.Schema({
+    title: String,
+    description: String,
+    published: Boolean,
+  });
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Template = mongoose.model("template", schema);
 
   return Template;
 };
